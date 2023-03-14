e83ca0ea725d3fd087c00c21ec659d03fbae8512
Liviu Rosu <liviurosu08@gmail.com>
2023-03-14 19:37:05 +0200

    first commit

---
+++ src/main/java/crudapis/restfulcrudbootproject22/Restfulcrudp2Application.java
@@ -0,0 +1,13 @@
+package crudapis.restfulcrudbootproject22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Restfulcrudp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Restfulcrudp2Application.class, args);
+    }
+
+}
---
+++ src/main/java/crudapis/restfulcrudbootproject22/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package crudapis.restfulcrudbootproject22.repository;
+
+import crudapis.restfulcrudbootproject22.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+    Product findByName(String name);
+}
